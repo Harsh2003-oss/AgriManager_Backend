f8a536182c463bd71c9c62597093d6c64b26733b
Harsh2003-oss <harsh.sil872@gmail.com>
2026-02-17 23:05:27 +0530

    chore: updated code

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin:'http://localhost:5173'
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true
 }))
 
 app.use('/', userRouter);
